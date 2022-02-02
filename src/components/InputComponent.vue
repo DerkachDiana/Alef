e2d4d8b61f751ffa9821b6b8352c87f3a2fcb937
qwerty <qwerty@mail.ru>
2022-02-02 22:49:54 +0300

    add header, footer, title and two raw  inputs

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "InputComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "TitleComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
